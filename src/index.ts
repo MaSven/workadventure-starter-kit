e6ecb686587cd74c11b79da94d509a4280c4fe9d
Sven Marquardt <dev@mail.smarquardt.space>
2021-12-14 22:07:32 +0100

    play sound from chat
    Signed-off-by: Sven Marquardt <dev@mail.smarquardt.space>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 //
 //
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 bootstrapExtra().catch((e: any) => {
     console.error(e);
@@ -11,3 +11,22 @@ bootstrapExtra().catch((e: any) => {
 console.log('Hello world!');
 
 WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+
+
+WA.chat.onChatMessage((message: string) => {
+    if (message.includes('Audio:')) {
+        const message_parts: string[] = message.split(':', 2);
+        const audio_file = message_parts[1];
+        const audio = WA.sound.loadSound(audio_file);
+        const config = {
+            volume: 0.5,
+            loop: false,
+            rate: 1,
+            detune: 1,
+            delay: 0,
+            seek: 0,
+            mute: false
+        };
+        audio.play(config);
+    }
+});
